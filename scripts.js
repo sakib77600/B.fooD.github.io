8c400494862976bbbcaa5a715f743d83a1f15657
sakib77600 <64540657+sakib77600@users.noreply.github.com>
2020-04-29 21:14:14 +0600

    Added a new files

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$(function (){
+    $('.hamburger-menu').on('click', function() {
+        $('.toggle').toggleClass('open');
+        $('.nav-list').toggleClass('open');
+    });
+    
+    AOS.init({
+        easing: 'ease',
+        duration: 1500,
+    });
+    
+    
+});
